49d5d3e6542ea36e8ce60d027653088dd7fce4f7
MammosGeorgios <mammosgeorgios@gmail.com>
2020-11-23 09:25:29 +0200

    Add project files.

---
+++ LinqSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\windows";
+            ShowLargeFilesWithoutLinq(path);
+
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name,-30} : {file.Length}");
+            }
+        }
+    }
+}
